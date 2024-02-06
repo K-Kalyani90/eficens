73fedfaecb557a0d667ac6abcee063533762e0f2
KalyaniKodali1 <kalyanikodali111@gmail.com>
2024-02-06 10:54:58 -0600

    eficens first commit

--- src/vehicle.java
+++ src/vehicle.java
@@ -1,3 +1,8 @@
-
 public class vehicle {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++)
+            System.out.println("i = " + i);
+    }
 }
